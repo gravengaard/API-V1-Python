0dbb998dc6f0dfb2905bfb2ea2b59b374cac481c
laisee <travis@coinmkt.com>
2014-04-21 19:26:43 +0800

    Update AddTrades.py
    remove extra logging messages, some hard-coded values

--- AddTrades.py
+++ AddTrades.py
@@ -12,7 +12,6 @@ sampleAmount = "1.01"
 samplePrice = "18.88"
 
 URL = "%s/%s/%s" % (CoinMktAPI.URL,"order/new/many", CoinMktAPI.SessionToken)
-print "URL : " + URL
 
 def trace_except(sysexecinfo, smessage = ''):
    """ Trace exceptions """
@@ -34,19 +33,7 @@ class Orders(object):
 order = Order()
 orders = Orders()
 
-# looks like this ...
-# [
-#    {"FromCoinSymbol":"LTC","ToCoinSymbol":"USD","TradeType":1,"TradeSide":1,"TradePrice":8.88,"Units":0.01,"TradeLevel":0,"SessionToken":"EzAerFa1CtEQJa6tov9t9Hta-vyNFYzaKQ6hxRV9pw9Hj1_P7VVU3aA7WWHz8k3b-PU_XRHsPBQzZ0Nin_KVJyzuhWI0Oa7tjnxjeTUEwEA%3D","ApiKey":"","IP":"49.128.60.181"},
-#    {"FromCoinSymbol":"BTC","ToCoinSymbol":"USD","TradeType":1,"TradeSide":1,"TradePrice":8.88,"Units":0.01,"TradeLevel":0,"SessionToken":"EzAerFa1CtEQJa6tov9t9Hta-vyNFYzaKQ6hxRV9pw9Hj1_P7VVU3aA7WWHz8k3b-PU_XRHsPBQzZ0Nin_KVJyzuhWI0Oa7tjnxjeTUEwEA%3D","ApiKey":"","IP":"49.128.60.181"},
-#    {"FromCoinSymbol":"FTC","ToCoinSymbol":"USD","TradeType":1,"TradeSide":1,"TradePrice":8.88,"Units":0.01,"TradeLevel":0,"SessionToken":"EzAerFa1CtEQJa6tov9t9Hta-vyNFYzaKQ6hxRV9pw9Hj1_P7VVU3aA7WWHz8k3b-PU_XRHsPBQzZ0Nin_KVJyzuhWI0Oa7tjnxjeTUEwEA%3D","ApiKey":"","IP":"49.128.60.181"},
-#    {"FromCoinSymbol":"FTC","ToCoinSymbol":"BTC","TradeType":1,"TradeSide":1,"TradePrice":8.88,"Units":0.01,"TradeLevel":0,"SessionToken":"EzAerFa1CtEQJa6tov9t9Hta-vyNFYzaKQ6hxRV9pw9Hj1_P7VVU3aA7WWHz8k3b-PU_XRHsPBQzZ0Nin_KVJyzuhWI0Oa7tjnxjeTUEwEA%3D","ApiKey":"","IP":"49.128.60.181"},
-#    {"FromCoinSymbol":"PPC","ToCoinSymbol":"USD","TradeType":1,"TradeSide":1,"TradePrice":8.88,"Units":0.01,"TradeLevel":0,"SessionToken":"EzAerFa1CtEQJa6tov9t9Hta-vyNFYzaKQ6hxRV9pw9Hj1_P7VVU3aA7WWHz8k3b-PU_XRHsPBQzZ0Nin_KVJyzuhWI0Oa7tjnxjeTUEwEA%3D","ApiKey":"","IP":"49.128.60.181"}
-# ]
-
-for x in range(0, 200):
-    order.AppId = "5511fa73-9f09-4dc0-a8a7-8e5a75160fc0"
-    #order.ApiKey = CoinMktAPI.ApiKey
-    order.IP = "49.128.60.181"
+for x in range(0, 20):
     order.FromCoinSymbol = CoinMktAPI.Currency
     order.ToCoinSymbol = "USD"
     order.TradePrice = round(random.uniform(1,3),3)
@@ -57,39 +44,20 @@ for x in range(0, 200):
     order.SessionToken = CoinMktAPI.SessionToken
     orders.list.append(order)
 
-#print "orders:"
-#for o in orders.list:
-#    print o.to_JSON()
-
 orderstr = json.dumps(orders.list, default=encodeOrder)
 length = len(orderstr)
-
-#print "Orders : " + orderstr
-
 myheaders = {'Content-Type': 'application/json','Content-Length': length}
-#print "Headers : " + str(myheaders) 
-
-#response = requests.post(URL,data=orderstr, headers=myheaders)
-#print response.status_code
-#print response.text
-#print response.url
-
 
 try:     
     request = urllib2.Request(url=URL,data=orderstr,headers=myheaders)
     request.get_method = lambda: 'POST'
-    print "d2 " + request.get_data()
-    
     response_body = urllib2.urlopen(request).read()
-
-    print "Response " + response_body
-
     data = json.loads(response_body)
     if data is not None: 
         print("Error Code   : " +str(data['Code']))
         print("Error string : " +data['Err'])
     else:    
-        print("Error - trade was not created ")
+        print("Error - trades not created ")
 
 except: # catch *all* exceptions
     error = trace_except(sys.exc_info())    
