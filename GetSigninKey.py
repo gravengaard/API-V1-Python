b581d92b7a9525430526ef7230ba7d5bf3c1efc4
laisee <travis@coinmkt.com>
2014-04-21 19:28:43 +0800

    Update GetSigninKey.py
    remove extra logging messages, hard-coded values

--- GetSigninKey.py
+++ GetSigninKey.py
@@ -6,19 +6,10 @@ import json
 
 URL = "%s/%s/%s" % (CoinMktAPI.URL,"session/signinkey",CoinMktAPI.ApiKey)
 
-print "URL : " + URL
 request = Request(URL)
 
 response_body = urlopen(request).read()
-print response_body
-
 data = json.loads(response_body)
-print("Key          : " +data['Key'])
-print("Error Code   : " +str(data['Code']))
-print("Error string : " + data['Err'])
-
 encryptedString = base64.b64decode(data['Key'])
-print("base64 string : " + encryptedString)
 cipher = DES3.new( CoinMktAPI.ApiSecret, DES3.MODE_ECB )
 decryptedString = cipher.decrypt(encryptedString)
-print "Decrypted string " + decryptedString
